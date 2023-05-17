812ec32f85f259bc5d6eb1ab00ea122836ff87aa
anikethgalla <111221827+anikethgalla@users.noreply.github.com>
2023-05-17 21:00:38 +0530

    Update Facedetect.py

--- Facedetect.py
+++ Facedetect.py
@@ -1,35 +1,15 @@
 import cv2
-import sys
+import play
 
-cascPath = sys.argv[1]
-faceCascade = cv2.CascadeClassifier(cascPath)
+image=cv2.imread("astro.jpg")
+#import xml file and reading xml file usign cascade classifier
+haarcascade=cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+#detecting face(only frontal face)
+faced=haarcascade.detectMultiScale(image,1.05,4)
+print(faced)
 
-video_capture = cv2.VideoCapture(0)
+for x,y,w,h in faced:
+  #creating a rectangle
+  cv2.rectangle(image,(x,y),(x+w,y+h),(0,255,0),3)
 
-while True:
-    # Capture frame-by-frame
-    ret, frame = video_capture.read()
-
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    faces = faceCascade.detectMultiScale(
-        gray,
-        scaleFactor=1.1,
-        minNeighbors=5,
-        minSize=(30, 30),
-        flags=cv2.cv.CV_HAAR_SCALE_IMAGE
-    )
-
-    # Draw a rectangle around the faces
-    for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-
-    # Display the resulting frame
-    cv2.imshow('Video', frame)
-
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-# When everything is done, release the capture
-video_capture.release()
-cv2.destroyAllWindows()
+cv2.imwrite("ouptu.png",image)
